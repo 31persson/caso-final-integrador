a25def817594c9b2e27747e8194f01450b9a8e9d
31persson <jperscas@myuax.com>
2023-11-28 12:21:27 +0100

    ejercicio

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <stdexcept>  // Agrega esta línea
 #include "json11/json11.hpp"
 
 enum variant_type { Symbol, Number, List, Proc, Lambda, Cadena };
@@ -30,11 +31,53 @@ public:
 };
 
 std::string Variant::to_string() const {
-    // Implementación de to_string
+    switch (type) {
+        case Symbol:
+        case Cadena:
+            return val;
+        case Number:
+            return std::to_string(std::stod(val));
+        case List: {
+            std::string result = "(";
+            for (const auto& elem : list) {
+                result += elem.to_string() + " ";
+            }
+            if (!list.empty()) {
+                result.pop_back(); // Eliminar el espacio adicional al final
+            }
+            result += ")";
+            return result;
+        }
+        case Proc:
+            return "<procedure>";
+        default:
+            return "Unknown type";
+    }
 }
 
 std::string Variant::to_json_string() const {
-    // Implementación de to_json_string
+    switch (type) {
+        case Symbol:
+        case Cadena:
+            return "\"" + val + "\"";
+        case Number:
+            return std::to_string(std::stod(val));
+        case List: {
+            std::string result = "[";
+            for (const auto& elem : list) {
+                result += elem.to_json_string() + ",";
+            }
+            if (!list.empty()) {
+                result.pop_back(); // Eliminar la coma adicional al final
+            }
+            result += "]";
+            return result;
+        }
+        case Proc:
+            return "\"<procedure>\"";
+        default:
+            return "\"Unknown type\"";
+    }
 }
 
 Variant Variant::from_json_string(const std::string& sjson) {
